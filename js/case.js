7409b6ac87e7dbc29aa47b721cf0f504bf91ab90
MD ALAMIN A <86530759+Alamin0x01@users.noreply.github.com>
2023-02-15 19:15:28 +0600

    Prettir Formatting added

--- js/case.js
+++ js/case.js
@@ -7,41 +7,42 @@
 
 */
 
-function updateCaseNumber(isIncrease){
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
+function updateCaseNumber(isIncrease) {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
 
-    let newCaseNumber;
+  let newCaseNumber;
 
-    if(isIncrease === true){
-        newCaseNumber = previousCaseNumber + 1;
-    }
-    else{
-        newCaseNumber = previousCaseNumber - 1;
-    }
+  if (isIncrease === true) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    newCaseNumber = previousCaseNumber - 1;
+  }
 
-    caseNumberField.value = newCaseNumber;
+  caseNumberField.value = newCaseNumber;
 
-    return newCaseNumber;
+  return newCaseNumber;
 }
 
-function updateCaseTotalPrice(newCaseNumber){
-    const caseTotalPrice = newCaseNumber * 59;
-    const caseTotalElement = document.getElementById('case-total');
-    caseTotalElement.innerText = caseTotalPrice;
-} 
+function updateCaseTotalPrice(newCaseNumber) {
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
+}
 
-document.getElementById('btn-case-plus').addEventListener('click', function(){
-    const newCaseNumber = updateCaseNumber(true);
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
 
-    updateCaseTotalPrice(newCaseNumber);
-    calculateSubTotal();
+  updateCaseTotalPrice(newCaseNumber);
+  calculateSubTotal();
 });
 
-document.getElementById('btn-case-minus').addEventListener('click', function(){
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
     const newCaseNumber = updateCaseNumber(false);
-    
+
     updateCaseTotalPrice(newCaseNumber);
     calculateSubTotal();
-})
+  });
--- js/common.js
+++ js/common.js
@@ -1,29 +1,28 @@
-
-function getTextElementValueById(elementId){
-    const phoneTotalElement = document.getElementById(elementId);
-    const currentPhoneTotalString = phoneTotalElement.innerText;
-    const currentPhoneTotal = parseInt(currentPhoneTotalString);
-    return currentPhoneTotal;
+function getTextElementValueById(elementId) {
+  const phoneTotalElement = document.getElementById(elementId);
+  const currentPhoneTotalString = phoneTotalElement.innerText;
+  const currentPhoneTotal = parseInt(currentPhoneTotalString);
+  return currentPhoneTotal;
 }
 
-function setTextElementValueById(elementId, value){
-    const subTotalElement = document.getElementById(elementId);
-    subTotalElement.innerText = value;
+function setTextElementValueById(elementId, value) {
+  const subTotalElement = document.getElementById(elementId);
+  subTotalElement.innerText = value;
 }
 
-function calculateSubTotal(){
-    // calculate total
-    const currentPhoneTotal = getTextElementValueById('phone-total');
-    const currentCaseTotal = getTextElementValueById('case-total');
-    
-    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-    setTextElementValueById('sub-total', currentSubTotal);
+function calculateSubTotal() {
+  // calculate total
+  const currentPhoneTotal = getTextElementValueById("phone-total");
+  const currentCaseTotal = getTextElementValueById("case-total");
 
-    // calculate tax
-    const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
-    const taxAmount = parseFloat(taxAmountString);
-    setTextElementValueById('tax-amount', taxAmount);
+  const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+  setTextElementValueById("sub-total", currentSubTotal);
 
-    const finalAmount = currentSubTotal + taxAmount;
-    setTextElementValueById('final-total', finalAmount);
+  // calculate tax
+  const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+  const taxAmount = parseFloat(taxAmountString);
+  setTextElementValueById("tax-amount", taxAmount);
+
+  const finalAmount = currentSubTotal + taxAmount;
+  setTextElementValueById("final-total", finalAmount);
 }
